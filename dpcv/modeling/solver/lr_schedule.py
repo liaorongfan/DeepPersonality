c262e4ee45b2878cb6630a221aa0047ea0a9fded
liaorongfan <15670381505@163.com>
2022-05-31 23:53:01 +0800

    update results

--- dpcv/modeling/solver/lr_schedule.py
+++ dpcv/modeling/solver/lr_schedule.py
@@ -8,5 +8,15 @@ def multi_step_scale(cfg, optimizer):
 
 
 @SOLVER_REGISTRY.register()
+def cosine_annealing(cfg, optimizer):
+    return optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=10, eta_min=0.0001)
+
+
+@SOLVER_REGISTRY.register()
+def exponential(cfg, optimizer):
+    return optim.lr_scheduler.ExponentialLR(optimizer, gamma=0.95)
+
+
+@SOLVER_REGISTRY.register()
 def crnet_multi_step_scale(cfg, optimizer):
     return optim.lr_scheduler.MultiStepLR(optimizer[1], gamma=cfg.SOLVER.FACTOR, milestones=cfg.SOLVER.MILESTONE)
