4352bf68170095b4b1cc6c8611e4f703fbe80007
liaorongfan <15670381505@163.com>
2022-06-26 00:43:31 +0800

    small modification to align with cloud

--- dpcv/modeling/solver/lr_schedule.py
+++ dpcv/modeling/solver/lr_schedule.py
@@ -9,12 +9,13 @@ def multi_step_scale(cfg, optimizer):
 
 @SOLVER_REGISTRY.register()
 def cosine_annealing(cfg, optimizer):
-    return optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=10, eta_min=0.0001)
+    return optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=20, eta_min=0.0001)
 
 
 @SOLVER_REGISTRY.register()
 def exponential(cfg, optimizer):
     return optim.lr_scheduler.ExponentialLR(optimizer, gamma=0.95)
+
 
 
 @SOLVER_REGISTRY.register()
