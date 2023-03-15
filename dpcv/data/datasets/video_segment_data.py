ca5df6aba89ecb7fe74b3573518c33abe588a8b4
rongfan <rongfan.liao@hotmail.com>
2023-03-15 23:30:03 +0800

    run resnet3d on all session

--- dpcv/data/datasets/video_segment_data.py
+++ dpcv/data/datasets/video_segment_data.py
@@ -330,6 +330,46 @@ def true_personality_spatial_temporal_data_loader(cfg, mode="train"):
     return data_loader
 
 
+@DATA_LOADER_REGISTRY.register()
+def all_true_personality_spatial_temporal_data_loader(cfg, mode="train"):
+
+    from torch.utils.data.dataset import ConcatDataset
+    spatial_transform = build_transform_spatial(cfg)
+    temporal_transform = [TemporalRandomCrop(32)]
+    # temporal_transform = [TemporalDownsample(length=2000), TemporalRandomCrop(16)]
+    temporal_transform = TemporalCompose(temporal_transform)
+
+    data_cfg = cfg.DATA
+
+    if data_cfg.TYPE == "face":
+        video_loader = VideoLoader(image_name_formatter=lambda x: f"face_{x}.jpg")
+    else:
+        video_loader = VideoLoader(image_name_formatter=lambda x: f"frame_{x}.jpg")
+        
+    datasets = []
+    for session in ["ghost", "animal", "talk", "lego"]:
+        data_set = TruePersonalityVideoFrameSegmentData(
+            data_root=data_cfg.ROOT,
+            data_split=mode,
+            task=session,
+            data_type=data_cfg.TYPE,
+            video_loader=video_loader,
+            spa_trans=spatial_transform,
+            tem_trans=temporal_transform,
+        )
+        datasets.append(data_set)
+    conca_datasets = ConcatDataset(datasets)
+    shuffle = True if mode == "train" else False
+    loader_cfg = cfg.DATA_LOADER
+    data_loader = DataLoader(
+        dataset=conca_datasets,
+        batch_size=loader_cfg.TRAIN_BATCH_SIZE,
+        shuffle=shuffle,
+        num_workers=loader_cfg.NUM_WORKERS,
+    )
+    return data_loader
+
+
 if __name__ == "__main__":
     import os
     from dpcv.config.default_config_opt import cfg
