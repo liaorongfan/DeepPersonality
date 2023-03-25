62393bc7c2eaac6c164f2868b0e841b681b3bf95
rongfan <rongfan.liao@hotmail.com>
2023-03-25 21:31:25 +0800

    update sigle trait training files for aud_crnet model

--- run.sh
+++ run.sh
@@ -6,8 +6,14 @@
 # python script/run_exp.py -c config/true_personality/single_trait/E/02_hrnet_face.yaml
 # python script/run_exp.py -c config/true_personality/single_trait/N/02_hrnet_face.yaml
 
-python script/run_exp.py -c config/true_personality/single_trait/O/07_vat_face_video_level.yaml
-python script/run_exp.py -c config/true_personality/single_trait/A/07_vat_face_video_level.yaml
-python script/run_exp.py -c config/true_personality/single_trait/C/07_vat_face_video_level.yaml
-python script/run_exp.py -c config/true_personality/single_trait/E/07_vat_face_video_level.yaml
-python script/run_exp.py -c config/true_personality/single_trait/N/07_vat_face_video_level.yaml
+# python script/run_exp.py -c config/true_personality/single_trait/O/07_vat_face_video_level.yaml
+# python script/run_exp.py -c config/true_personality/single_trait/A/07_vat_face_video_level.yaml
+# python script/run_exp.py -c config/true_personality/single_trait/C/07_vat_face_video_level.yaml
+# python script/run_exp.py -c config/true_personality/single_trait/E/07_vat_face_video_level.yaml
+# python script/run_exp.py -c config/true_personality/single_trait/N/07_vat_face_video_level.yaml
+
+python script/run_exp.py -c config/true_personality/single_trait/O/03_aud_crnet.yaml
+python script/run_exp.py -c config/true_personality/single_trait/C/03_aud_crnet.yaml
+python script/run_exp.py -c config/true_personality/single_trait/E/03_aud_crnet.yaml
+python script/run_exp.py -c config/true_personality/single_trait/A/03_aud_crnet.yaml
+python script/run_exp.py -c config/true_personality/single_trait/N/03_aud_crnet.yaml
