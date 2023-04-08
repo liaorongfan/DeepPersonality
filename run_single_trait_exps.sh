1125c6daef0c1de3c2fefff4d5ac54ac8d4b7724
rongfan <rongfan.liao@hotmail.com>
2023-04-08 23:54:08 +0800

    finish ture presonality short clip  experiments

--- run_single_trait_exps.sh
+++ run_single_trait_exps.sh
@@ -1,49 +1,49 @@
 #! /bin/bash
 
-# python script/run_exp.py -c config/true_personality/single_trait/O/02_hrnet_face.yaml
-# python script/run_exp.py -c config/true_personality/single_trait/A/02_hrnet_face.yaml
-# python script/run_exp.py -c config/true_personality/single_trait/C/02_hrnet_face.yaml
-# python script/run_exp.py -c config/true_personality/single_trait/E/02_hrnet_face.yaml
-# python script/run_exp.py -c config/true_personality/single_trait/N/02_hrnet_face.yaml
+# python tools/run_exp.py -c config/true_personality/single_trait/O/02_hrnet_face.yaml
+# python tools/run_exp.py -c config/true_personality/single_trait/A/02_hrnet_face.yaml
+# python tools/run_exp.py -c config/true_personality/single_trait/C/02_hrnet_face.yaml
+# python tools/run_exp.py -c config/true_personality/single_trait/E/02_hrnet_face.yaml
+# python tools/run_exp.py -c config/true_personality/single_trait/N/02_hrnet_face.yaml
 
-# python script/run_exp.py -c config/true_personality/single_trait/O/07_vat_face_video_level.yaml
-# python script/run_exp.py -c config/true_personality/single_trait/A/07_vat_face_video_level.yaml
-# python script/run_exp.py -c config/true_personality/single_trait/C/07_vat_face_video_level.yaml
-# python script/run_exp.py -c config/true_personality/single_trait/E/07_vat_face_video_level.yaml
-# python script/run_exp.py -c config/true_personality/single_trait/N/07_vat_face_video_level.yaml
+# python tools/run_exp.py -c config/true_personality/single_trait/O/07_vat_face_video_level.yaml
+# python tools/run_exp.py -c config/true_personality/single_trait/A/07_vat_face_video_level.yaml
+# python tools/run_exp.py -c config/true_personality/single_trait/C/07_vat_face_video_level.yaml
+# python tools/run_exp.py -c config/true_personality/single_trait/E/07_vat_face_video_level.yaml
+# python tools/run_exp.py -c config/true_personality/single_trait/N/07_vat_face_video_level.yaml
 
-# python script/run_exp.py -c config/true_personality/single_trait/O/03_aud_crnet.yaml
-# python script/run_exp.py -c config/true_personality/single_trait/C/03_aud_crnet.yaml
-# python script/run_exp.py -c config/true_personality/single_trait/E/03_aud_crnet.yaml
-# python script/run_exp.py -c config/true_personality/single_trait/A/03_aud_crnet.yaml
-# python script/run_exp.py -c config/true_personality/single_trait/N/03_aud_crnet.yaml
-
-
-# python script/run_exp.py -c config/true_personality/single_trait/O/06_multi_modal_pred_audio.yaml
-# python script/run_exp.py -c config/true_personality/single_trait/C/06_multi_modal_pred_audio.yaml
-# python script/run_exp.py -c config/true_personality/single_trait/E/06_multi_modal_pred_audio.yaml
-# python script/run_exp.py -c config/true_personality/single_trait/A/06_multi_modal_pred_audio.yaml
-# python script/run_exp.py -c config/true_personality/single_trait/N/06_multi_modal_pred_audio.yaml
+# python tools/run_exp.py -c config/true_personality/single_trait/O/03_aud_crnet.yaml
+# python tools/run_exp.py -c config/true_personality/single_trait/C/03_aud_crnet.yaml
+# python tools/run_exp.py -c config/true_personality/single_trait/E/03_aud_crnet.yaml
+# python tools/run_exp.py -c config/true_personality/single_trait/A/03_aud_crnet.yaml
+# python tools/run_exp.py -c config/true_personality/single_trait/N/03_aud_crnet.yaml
 
 
-# python script/run_exp.py -c config/true_personality/single_trait/O/07_vat_face_video_level.yaml 
-# python script/run_exp.py -c config/true_personality/single_trait/C/07_vat_face_video_level.yaml 
-# python script/run_exp.py -c config/true_personality/single_trait/E/07_vat_face_video_level.yaml 
-# python script/run_exp.py -c config/true_personality/single_trait/A/07_vat_face_video_level.yaml 
-# python script/run_exp.py -c config/true_personality/single_trait/N/07_vat_face_video_level.yaml 
+# python tools/run_exp.py -c config/true_personality/single_trait/O/06_multi_modal_pred_audio.yaml
+# python tools/run_exp.py -c config/true_personality/single_trait/C/06_multi_modal_pred_audio.yaml
+# python tools/run_exp.py -c config/true_personality/single_trait/E/06_multi_modal_pred_audio.yaml
+# python tools/run_exp.py -c config/true_personality/single_trait/A/06_multi_modal_pred_audio.yaml
+# python tools/run_exp.py -c config/true_personality/single_trait/N/06_multi_modal_pred_audio.yaml
 
 
-# python script/run_exp.py -c config/true_personality/single_trait/O/12_crnet.yaml
-# python script/run_exp.py -c config/true_personality/single_trait/C/12_crnet.yaml
-# python script/run_exp.py -c config/true_personality/single_trait/E/12_crnet.yaml
-# python script/run_exp.py -c config/true_personality/single_trait/A/12_crnet.yaml
-# python script/run_exp.py -c config/true_personality/single_trait/N/12_crnet.yaml
+# python tools/run_exp.py -c config/true_personality/single_trait/O/07_vat_face_video_level.yaml 
+# python tools/run_exp.py -c config/true_personality/single_trait/C/07_vat_face_video_level.yaml 
+# python tools/run_exp.py -c config/true_personality/single_trait/E/07_vat_face_video_level.yaml 
+# python tools/run_exp.py -c config/true_personality/single_trait/A/07_vat_face_video_level.yaml 
+# python tools/run_exp.py -c config/true_personality/single_trait/N/07_vat_face_video_level.yaml 
 
 
-python script/run_exp.py -c config/true_personality/single_trait/O/14_multi_modal_pred_face.yaml
-python script/run_exp.py -c config/true_personality/single_trait/C/14_multi_modal_pred_face.yaml
-python script/run_exp.py -c config/true_personality/single_trait/E/14_multi_modal_pred_face.yaml
-python script/run_exp.py -c config/true_personality/single_trait/A/14_multi_modal_pred_face.yaml
-python script/run_exp.py -c config/true_personality/single_trait/N/14_multi_modal_pred_face.yaml
+# python tools/run_exp.py -c config/true_personality/single_trait/O/12_crnet.yaml
+# python tools/run_exp.py -c config/true_personality/single_trait/C/12_crnet.yaml
+# python tools/run_exp.py -c config/true_personality/single_trait/E/12_crnet.yaml
+# python tools/run_exp.py -c config/true_personality/single_trait/A/12_crnet.yaml
+# python tools/run_exp.py -c config/true_personality/single_trait/N/12_crnet.yaml
+
+
+python tools/run_exp.py -c config/true_personality/single_trait/O/14_multi_modal_pred_face.yaml
+python tools/run_exp.py -c config/true_personality/single_trait/C/14_multi_modal_pred_face.yaml
+python tools/run_exp.py -c config/true_personality/single_trait/E/14_multi_modal_pred_face.yaml
+python tools/run_exp.py -c config/true_personality/single_trait/A/14_multi_modal_pred_face.yaml
+python tools/run_exp.py -c config/true_personality/single_trait/N/14_multi_modal_pred_face.yaml
 
 
