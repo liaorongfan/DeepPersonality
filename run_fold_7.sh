e448ebc9325b7e94f982a8653945530042e0dcf7
rongfan <rongfan.liao@hotmail.com>
2023-04-24 01:52:53 +0800

    train true personality fold8 and fold9

--- run_fold_7.sh
+++ run_fold_7.sh
@@ -1,6 +1,6 @@
-python tools/run_exp.py -c config/true_personality/all_sessions_fold_7/03_aud_crnet.yaml
-python tools/run_exp.py -c config/true_personality/all_sessions_fold_7/06_multi_modal_pred_audio.yaml
+# python tools/run_exp.py -c config/true_personality/all_sessions_fold_7/03_aud_crnet.yaml
+# python tools/run_exp.py -c config/true_personality/all_sessions_fold_7/06_multi_modal_pred_audio.yaml
 python tools/run_exp.py -c config/true_personality/all_sessions_fold_7/07_vat_face_video_level.yaml
 python tools/run_exp.py -c config/true_personality/all_sessions_fold_7/12_crnet.yaml
-python tools/run_exp.py -c config/true_personality/all_sessions_fold_7/14_multi_modal_pred_face.yaml
+# python tools/run_exp.py -c config/true_personality/all_sessions_fold_7/14_multi_modal_pred_face.yaml
 python tools/run_exp.py -c config/true_personality/all_sessions_fold_7/02_hrnet_face.yaml
