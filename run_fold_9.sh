1e865186779c39015e0132c9602c47bf22e0abc6
rongfan <rongfan.liao@hotmail.com>
2023-04-30 01:32:28 +0800

    organize bash files

--- run_fold_9.sh
+++
@@ -1,6 +0,0 @@
-python tools/run_exp.py -c config/true_personality/all_sessions_fold_9/03_aud_crnet.yaml
-python tools/run_exp.py -c config/true_personality/all_sessions_fold_9/06_multi_modal_pred_audio.yaml
-python tools/run_exp.py -c config/true_personality/all_sessions_fold_9/07_vat_face_video_level.yaml
-python tools/run_exp.py -c config/true_personality/all_sessions_fold_9/14_multi_modal_pred_face.yaml
-python tools/run_exp.py -c config/true_personality/all_sessions_fold_9/02_hrnet_face.yaml
-python tools/run_exp.py -c config/true_personality/all_sessions_fold_9/12_crnet.yaml
