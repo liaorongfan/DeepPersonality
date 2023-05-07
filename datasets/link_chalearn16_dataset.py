d47ceb1078ecbd8d36bd7a64ebd31d3b32b7a890
liao <you@example.com>
2023-05-07 11:47:21 +0800

    train ip fold 7

--- datasets/link_chalearn16_dataset.py
+++ datasets/link_chalearn16_dataset.py
@@ -167,6 +167,6 @@ if __name__ == "__main__":
     # )
     link_folding = LinkChalearn16Folding(
         source_data_root="/root/DeepPersonality/datasets",
-        dest_data_root="datasets/folds/fold_6",
+        dest_data_root="datasets/folds/fold_7",
     )
-    link_folding.setup_one_fold_dataset(6)
+    link_folding.setup_one_fold_dataset(7)
