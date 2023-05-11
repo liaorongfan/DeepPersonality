fbeb93354bb86dd3cda37b27e136cb1a978e2c40
liao <you@example.com>
2023-05-12 02:48:18 +0800

    add multi-scale transform for strong img-preprocess

--- dpcv/data/transforms/transform.py
+++ dpcv/data/transforms/transform.py
@@ -37,9 +37,9 @@ def strong_frame_transform():
     transforms = transforms.Compose([
         transforms.Resize(256),
         transforms.RandomHorizontalFlip(0.5),
-        transforms.RandomRotation(10),
-        transforms.ColorJitter(brightness=0.1, saturation=0.1),
-        transforms.CenterCrop((224, 224)),
+        transforms.RandomRotation(15),
+        transforms.ColorJitter(brightness=0.2, saturation=0.2),
+        transforms.RandomResizedCrop(size=224, scale=(0.5, 1)),
         transforms.ToTensor(),
         transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
     ])
