f16fd124dd585696a3e97f16a5d2721ca1320791
rongfan <rongfan.liao@hotmail.com>
2023-06-11 15:55:48 +0800

    update

--- long_context.sh
+++ long_context.sh
@@ -1,8 +1,13 @@
-# 32 frames
-# python tools/run_exp.py -c config/impression/long_context/04_resnet50_3d_face.yaml
-# python tools/run_exp.py -c config/impression/long_context/06_tpn_face.yaml
-# python tools/run_exp.py -c config/impression/long_context/07_vat_face.yaml
+# 16 frames
+# python tools/run_exp.py -c config/impression/16_frames/long_context/04_resnet50_3d_face.yaml
+# python tools/run_exp.py -c config/impression/16_frames/long_context/06_tpn_face.yaml
+# python tools/run_exp.py -c config/impression/16_frames/long_context/07_vat_face.yaml
 
-python tools/run_exp.py -c config/true_personality/long_context/04_resnet3d_face.yaml
-python tools/run_exp.py -c config/true_personality/long_context/06_tpn_face.yaml
-python tools/run_exp.py -c config/true_personality/long_context/07_vat_face_video_level.yaml
+# python tools/run_exp.py -c config/true_personality/16_frames/long_context/04_resnet3d_face.yaml
+# python tools/run_exp.py -c config/true_personality/16_frames/long_context/06_tpn_face.yaml
+# python tools/run_exp.py -c config/true_personality/16_frames/long_context/07_vat_face_video_level.yaml
+
+# 64 frames 
+python tools/run_exp.py -c config/true_personality/long_context/64_frames/04_resnet3d_face_video_level.yaml
+python tools/run_exp.py -c config/true_personality/long_context/64_frames/06_tpn_face_video_level.yaml
+python tools/run_exp.py -c config/true_personality/long_context/64_frames/07_vat_face_video_level.yaml
